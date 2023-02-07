9a852c2e10e34da71965bff082e751bafdd4f2a7
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-07 15:24:31 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main  {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int[] cnt = new int[N];
+		int ans = 0;
+		int idx = 0;
+		cnt[0]++;
+		while (true) {
+			if (cnt[idx] == M) break;
+			if (cnt[idx]%2 == 1) {
+				idx = (idx+L)%N;
+				cnt[idx]++;
+				ans++;
+			}
+			else {
+				idx = (idx+N-L)%N;
+				cnt[idx]++;
+				ans++;
+			}
+		}
+		
+		bw.write(String.valueOf(ans));
+		bw.flush();
+		bw.close();
+		
+	}
+}
