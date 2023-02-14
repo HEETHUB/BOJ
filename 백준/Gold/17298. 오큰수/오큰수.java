0363e7a293a4e263b9876265b5f7f1d8d9f3aaa1
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-15 01:51:44 +0900

    [Gold IV] Title: 오큰수, Time: 1048 ms, Memory: 141808 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] A = new int[N];
@@ -33,8 +31,7 @@ public class Main {
 		}
 		
 		for (int a : ans)
-			bw.write(String.valueOf(a)+" ");
-		bw.flush();
-		bw.close();
+			sb.append(a).append(" ");
+		System.out.println(sb);
 	}
 }
