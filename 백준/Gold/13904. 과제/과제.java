bae92ba9c0aca9dfda80bf7dccda504a502c9dfd
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-17 03:47:55 +0900

    [Gold III] Title: 과제, Time: 140 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Integer[][] arr = new Integer[N][2];
+
+		int max = 0;
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, arr[i][0]);
+		}
+
+		Arrays.sort(arr, new Comparator<Integer[]>() {
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				return o2[1] - o1[1];
+			}
+		});
+
+		int[] day = new int[max + 1];
+
+		for (Integer[] work : arr) {
+			for (int j = work[0]; j >= 1; j--) {
+				if (day[j] == 0) {
+					day[j] = work[1];
+					break;
+				}
+			}
+		}
+		int ans = 0;
+		for (int d : day)
+			ans += d;
+		System.out.println(ans);
+	}
+}
