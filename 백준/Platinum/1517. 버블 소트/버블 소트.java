0bbbf7b15fbc437a588c10f622d82d5a914a403e
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-17 19:38:21 +0900

    [Platinum V] Title: 버블 소트, Time: 588 ms, Memory: 77896 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static long cnt = 0;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long[] arr = new long[N];
+		
+		for (int i = 0; i < N; i++)
+			arr[i] = Long.parseLong(st.nextToken());
+		mergeSort(arr);
+		System.out.println(cnt);
+	}
+	
+	static long[] sorted;
+	
+	public static void mergeSort(long[] arr) {
+		sorted = new long[arr.length];
+		mergeSortHelp(arr, 0, arr.length-1);
+		sorted = null;
+	}
+	
+	private static void mergeSortHelp(long[] arr, int left, int right) {
+		if (left == right) return;
+		
+		int mid = (left + right)/2;
+		
+		mergeSortHelp(arr, left, mid);
+		mergeSortHelp(arr, mid+1, right);
+		
+		merge(arr, left, mid, right);
+	}
+	
+	private static void merge(long[] arr, int left, int mid, int right) {
+		int l = left;
+		int r = mid+1;
+		int idx = left;
+		
+		while (l <= mid && r <= right) {
+			if (arr[l] <= arr[r]) {
+				sorted[idx++] = arr[l++];
+			}
+			else {
+				cnt += r - idx;
+				sorted[idx++] = arr[r++];
+			}
+				
+		}
+		
+		while (l <= mid) {
+			sorted[idx++] = arr[l++];
+		}
+		
+		while (r <= right) {
+			sorted[idx++] = arr[r++];
+		}
+		
+		for (int i = left; i <= right; i++)
+			arr[i] = sorted[i];
+	}	
+}
