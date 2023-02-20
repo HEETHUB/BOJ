35abf21e65b5ad50c708cc005dffc40c2e48f597
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-20 15:34:11 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 12332 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[][] maze;
+	static int cnt = 0;
+	static int[] dr = {0, 0, 1, -1};
+	static int[] dc = {1, -1, 0, 0};
+	static boolean[][] visited;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		maze = new int[N][M];
+		visited = new boolean[N][M];
+	
+		for (int i = 0; i < N; i++) {
+			String m = br.readLine();
+			for (int j = 0; j < M; j++)
+				maze[i][j] = m.charAt(j) - '0';
+		}
+		
+		bfs(0, 0, 1);
+		
+		System.out.println(maze[N-1][M-1]);
+	}
+
+	private static void bfs(int i, int j, int depth) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(i);
+		queue.offer(j);
+		
+		while (!queue.isEmpty()) {
+			int r = queue.poll(); 
+			int c = queue.poll();
+			
+			if (r == N-1 && c == M-1) {
+				break;
+			}
+			
+			for (int k = 0; k < 4; k++) {
+				int nr = r + dr[k];
+				int nc = c + dc[k];
+				if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+					if (maze[nr][nc] == 1) {
+						queue.offer(nr);
+						queue.offer(nc);
+						maze[nr][nc] = maze[r][c] + 1;
+					}
+				}
+			}
+		}
+	}
+}
