96d8931b72770e1e892e38b1d452b12a0b60b3d2
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-20 14:05:46 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 136 ms, Memory: 13284 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		StringBuilder sb= new StringBuilder();
 		int N = sc.nextInt();
 		int[] arr = new int[N];
 		for (int n = 0; n < N; n++) {
@@ -19,32 +18,38 @@ public class Main {
 			if (gender == 1) {
 				for (int j = 0; j < N; j++) {
 					if ((j + 1) % number == 0) {
-						change(arr, j);
+						if (arr[j] == 0)
+							arr[j] = 1;
+						else
+							arr[j] = 0;
 					}
 				}
-			} else if (gender == 2) {
-				change(arr, number - 1);
-				for (int k = 1; number - 1 + k < arr.length && number - 1 - k >= 0; k++) {
-					if (arr[number - 1 + k] == arr[number - 1 - k]) {
-						change(arr, number - 1 - k);
-						change(arr, number - 1 + k);
-					} else
+			}
+			if (gender == 2) {
+				int k = 0;
+				while (true) {
+					if (number + k -1 >= N || number - k -1 < 0)
+						break;
+					if (arr[number - 1 - k] == arr[number - 1 + k]) {
+						if (arr[number - 1 - k] == 1) {
+							arr[number - 1 - k] = 0;
+							arr[number - 1 + k] = 0;
+							k++;
+						} else if (arr[number - 1 - k] == 0) {
+							arr[number - 1 - k] = 1;
+							arr[number - 1 + k] = 1;
+							k++;
+						}
+					}
+					else
 						break;
 				}
 			}
 
 		}
-		for (int i = 0; i < N; i++) {
-			sb.append(arr[i]+" ");
-			if ((i + 1)%20 == 0) sb.append("\n");
+		for(int i =0; i<arr.length;i++) {
+			System.out.print(arr[i] + " ");
+			if ((i+1)%20 == 0) System.out.print("\n");
 		}
-		System.out.println(sb);
-	}
-
-	private static void change(int[] arr, int i) {
-		if (arr[i] == 0)
-			arr[i] = 1;
-		else
-			arr[i] = 0;
 	}
 }
