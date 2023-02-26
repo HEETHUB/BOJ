3872c049e11ee87f4af122f64c285103d8abc54a
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-27 00:32:58 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 132 ms, Memory: 13296 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
