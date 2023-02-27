c56773a9d84bcb3a5bd95a1481eb7ff643d20d29
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-27 13:38:34 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 120 ms, Memory: 12904 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,12 +9,12 @@ public class Main {
 		for (long i = 2; i <= Math.sqrt(n); i++) {
 			if (n%i == 0) {
 				ans -= ans/i;
-				while (n%i == 0)
-					n /= i;
+				while (n%i == 0) n /= i;
 			}
 		}
-		if (n > 1)
-			ans -= ans/n;
-		System.out.print(ans+" ");
+		
+		if (n > 1) ans -= ans/n;
+		
+		System.out.println(ans);
 	}
 }
