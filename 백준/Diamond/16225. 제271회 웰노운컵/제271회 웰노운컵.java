88c76d7c47199ef14e157744b8eee3e36abc436e
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-27 22:08:44 +0900

    [Diamond V] Title: 제271회 웰노운컵, Time: 728 ms, Memory: 84728 KB -BaekjoonHub

--- 백준/Diamond/16225. 제271회 웰노운컵/제271회 웰노운컵.java
+++ 백준/Diamond/16225. 제271회 웰노운컵/제271회 웰노운컵.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,7 +9,6 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 
 		node[] arr = new node[N];
@@ -48,9 +45,7 @@ public class Main {
 			pq2.add(pq1.poll().A);
 			ans += pq2.poll();
 		}
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
+		System.out.println(ans);
 	}
 
 	static class node {
