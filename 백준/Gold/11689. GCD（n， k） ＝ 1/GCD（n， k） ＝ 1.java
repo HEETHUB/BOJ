0238a17c2fb55387acadb2ce8bfe63722e17c6a5
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-02-27 13:40:01 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 88 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		long n = sc.nextLong();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
 		long ans = n;
 		
 		for (long i = 2; i <= Math.sqrt(n); i++) {
