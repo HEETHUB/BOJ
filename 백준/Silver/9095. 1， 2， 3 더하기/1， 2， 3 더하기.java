d791f530976601e2868f62752ba5288bde40de23
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-03-23 16:36:39 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,36 +1,20 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
-	static int n;
-	static int ans;
-	static void makeN(int num) {
-		if (num == n) {
-			ans++;
-			return;
-		}
-		if (num+3 <= n) makeN(num+3);
-		if (num+2 <= n) makeN(num+2);
-		if (num+1 <= n) makeN(num+1);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int T = sc.nextInt();
 		
 		for (int t = 1; t <= T; t++) {
-			n = Integer.parseInt(br.readLine());
-			ans = 0;
-			
-			makeN(0);
-			bw.write(String.valueOf(ans));
-			bw.newLine();
-			
+			int n = sc.nextInt();
+			int[] dp = new int[n+3];
+			dp[2] = 1;
+			for (int i = 3; i <= n+2; i++) {
+				dp[i] += dp[i-1] + dp[i-2] + dp[i-3];
+			}
+			sb.append(dp[n+2]+"\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
