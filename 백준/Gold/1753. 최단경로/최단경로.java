621f8d2917a92289520cb29b46c86adcb3eb1b56
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-03-24 15:15:33 +0900

    [Gold IV] Title: 최단경로, Time: 712 ms, Memory: 119300 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -3,68 +3,66 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer VE = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(VE.nextToken());
 		int E = Integer.parseInt(VE.nextToken());
 		int start = Integer.parseInt(br.readLine());
 		
-		ArrayList<node>[] graph = new ArrayList[V+1];
-		
-		for (int i = 0; i < V+1; i++) 
+		ArrayList<edge>[] graph = new ArrayList[V+1];
+		for (int i = 0; i <= V; i++)
 			graph[i] = new ArrayList<>();
 		
 		for (int i = 0; i < E; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			graph[u].add(new node(v, w));
+			graph[s].add(new edge(e, w));
 		}
 		
 		int[] distance = new int[V+1];
-		for (int i = 0; i <= V; i++)
-			distance[i] = Integer.MAX_VALUE;
-		
+		boolean[] visited = new boolean[V+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
 		distance[start] = 0;
 		
-		PriorityQueue<node> pq = new PriorityQueue<>(new Comparator<node>() {
-			@Override
-			public int compare(node o1, node o2) {
-				return o1.w - o2.w;
-			} 
-		});
+		PriorityQueue<edge> pq = new PriorityQueue<>();
+		pq.add(new edge(start, 0));
 		
-		boolean[] visited = new boolean[V+1];
-//		visited[start] = true;
-		pq.add(new node(start, 0));
 		while (!pq.isEmpty()) {
-			node cur = pq.poll();
-			if (visited[cur.v]) continue;
-			visited[cur.v] = true;
-			for (node next : graph[cur.v]) {
-				if (distance[next.v] > distance[cur.v] + next.w) {
-//					visited[next.v] = true;
-					distance[next.v] = distance[cur.v] + next.w;
-					pq.add(new node(next.v, distance[next.v]));
+			edge cur = pq.poll();
+			
+			if (visited[cur.e]) continue;
+			visited[cur.e] = true;
+			
+			for (edge next : graph[cur.e]) {
+				if (distance[next.e] > distance[cur.e] + next.w) {
+					distance[next.e] = distance[cur.e] + next.w;
+					pq.add(new edge(next.e, distance[next.e]));
 				}
 			}
 		}
-		
-		for (int i = 1; i < V+1; i++) {
-			if (!visited[i]) System.out.println("INF");
-			else System.out.println(distance[i]);
+		for (int i = 1; i <= V; i++) {
+			if (distance[i] == Integer.MAX_VALUE) sb.append("INF\n");
+			else sb.append(distance[i]+"\n");
 		}
+//		System.out.println(Arrays.toString(distance));
 		System.out.println(sb);
 	}
-}
+	
+	static class edge implements Comparable<edge>{
+		int e;
+		int w;
+		
+		public edge(int e, int w) {
+			this.e = e;
+			this.w = w;
+		}
 
-class node{
-	int v;
-	int w;
-	public node(int v, int w) {
-		this.v = v;
-		this.w = w;
+		@Override
+		public int compareTo(edge o) {
+			return this.w - o.w;
+		}
 	}
 }
