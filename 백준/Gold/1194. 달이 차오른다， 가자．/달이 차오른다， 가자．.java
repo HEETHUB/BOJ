e84d9097dd316182f653adda38fe897846bec1e0
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-04-06 16:26:21 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 108 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -21,7 +21,7 @@ public class Main {
 			String line = br.readLine();
 			map[i] = line.toCharArray();
 			for (int j = 0; j < M; j++) {
-				if (map[i][j] == '0') start = new Node(i, j, 0);
+				if (map[i][j] == '0') start = new Node(i, j, 0, 0);
 			}
 		}
 
@@ -30,31 +30,29 @@ public class Main {
 
 		while (!queue.isEmpty()) {
 			Node cur = queue.poll();
-			int keyIdx = checkKeys(cur.keys);
 			
 			for (int k = 0; k < 4; k++) {
 				int nr = cur.r + dr[k];
 				int nc = cur.c + dc[k];
-				if (check(nr, nc) && map[nr][nc] != '#' && !visited[nr][nc][keyIdx]) {
-					Node next = new Node(nr, nc, cur.cnt+1);
-					next.keys = Arrays.copyOfRange(cur.keys, 0, 6);
+				if (check(nr, nc) && map[nr][nc] != '#' && !visited[nr][nc][cur.key]) {
+					Node next = new Node(nr, nc, cur.cnt+1, cur.key);
 					if (map[nr][nc] == '1') {
-						System.out.println(cur.cnt+1);
+						System.out.println(next.cnt);
 						return;
 					}
 					else if (map[nr][nc] == '0' || map[nr][nc] == '.'){
-						visited[nr][nc][keyIdx] = true;
+						visited[nr][nc][cur.key] = true;
 						queue.add(next);
 					} 
 					else if (map[nr][nc] >= 'A' && map[nr][nc] <= 'F'){
-						if (cur.keys[map[nr][nc] - 'A'] > 0) {
-							visited[nr][nc][keyIdx] = true;
+						if ((next.key & (1 << (map[nr][nc] - 'A'))) > 0) {
+							visited[nr][nc][next.key] = true;
 							queue.add(next);
 						}
 					} 
 					else if (map[nr][nc] >= 'a' && map[nr][nc] <= 'f') {
-						next.keys[map[nr][nc]-'a'] = 1;
-						visited[nr][nc][keyIdx] = true;
+						visited[nr][nc][cur.key] = true;
+						next.key = next.key | (1 << (map[nr][nc]-'a'));
 						queue.add(next);
 					}
 				}
@@ -72,19 +70,13 @@ public class Main {
 		int r;
 		int c;
 		int cnt;
-		int[] keys = new int[6];
+		int key = 0;
 		
-		public Node(int r, int c, int cnt) {
+		public Node(int r, int c, int cnt, int key) {
 			this.r = r;
 			this.c = c;
 			this.cnt = cnt;
+			this.key = key;
 		}
-	}
-	
-	private static int checkKeys(int[] arr) {
-		int ans = 0;
-		for (int i = 0; i < 6; i++) 
-			ans += Math.pow(2, i)*arr[i];
-		return ans;
 	}
 }
