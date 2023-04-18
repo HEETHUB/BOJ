315d800fb04a16f4fc1ddeb5349eb6102d3542c2
Cho Heetae <113952597+HEETHUB@users.noreply.github.com>
2023-04-18 16:48:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,6 @@
+a,b,c = map(int, input().split())
+if (a == b and b == c) : print(10000+1000*a)
+elif (a == b) : print(1000+100*a)
+elif (b == c) : print(1000+100*b)
+elif (c == a) : print(1000+100*c)
+else : print(100*(max(a, b, c)))
